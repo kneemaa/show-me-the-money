cebb705099d1172b1dccc9f4dde57b962d2a2258
CDF Sven <chendf999@gmail.com>
2018-03-19 20:42:40 -0700

    add portfolio diagram

---
+++ public/javascript/pie-chart.js
@@ -0,0 +1,48 @@
+var dom = document.getElementById("portfolio");
+var myChart = echarts.init(dom);
+var app = {};
+
+option = {
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b}: {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        x: '50px',
+        y: 'center',
+        data:['Stock1','Stock2','Stock3','Stock4','Stock5', 'Stock6']
+    },
+    series: [
+        {
+            name:'Distribution',
+            type:'pie',
+            radius: ['40%', '50%'],
+            avoidLabelOverlap: false,
+            label: {
+                normal: {
+                    show: false,
+                    position: 'center'
+                },
+                emphasis: {
+                    show: true,
+                    textStyle: {
+                        fontSize: '20',
+                        fontWeight: 'bold'
+                    }
+                }
+            },
+            data:[
+                {value:335, name:'Stock1'},
+                {value:310, name:'Stock2'},
+                {value:234, name:'Stock3'},
+                {value:135, name:'Stock4'},
+                {value:1548, name:'Stock5'},
+                {value:1218, name:'Stock6'}
+            ]
+        }
+    ],
+    color: ['#e3f2fd', '#bbdefb','#90caf9','#42a5f5', '#1e88e5', '#0d47a1']
+};
+
+myChart.setOption(option, true);
