0c4456b87eb1d65a94a488fc3957fca30ee9151a
Nema Darban <nema.darban@gmail.com>
2018-03-24 12:59:51 -0700

    fixed url and sign up

--- routes/index.js
+++ routes/index.js
@@ -22,14 +22,14 @@ const checkLoggedIn = async (user) => {
   const lookUp = async (user) => {
     return requestPromise({
       method: 'get',
-      url: env.AUTH0_CALLBACK_URL+'/api/user/' + user.nickname + "@gmail.com"
+      url: 'http://localhost:3000/api/user/' + user.nickname + "@gmail.com"
     }).then(result => {
       console.log(result)
       if(result === "[]"){
         const createUser = (user) => {
           return requestPromise({
             method: "POST",
-            url: "/api/user/",
+            url: "http://localhost:3000/api/user/",
             json: {
                 first_name: user.name.givenName,
                 last_name: user.name.familyName,
@@ -56,7 +56,7 @@ const checkLoggedIn = async (user) => {
     const lookUp = async () => {
       return requestPromise({
         method: 'get',
-        url: env.AUTH0_CALLBACK_URL+'/api/user/' + user.nickname + "@gmail.com"
+        url: 'http://localhost:3000/api/user/' + user.nickname + "@gmail.com"
       }).then(result => {
         result = JSON.parse(result)
         return result[0].id
@@ -68,7 +68,7 @@ const checkLoggedIn = async (user) => {
     const portfolio = () => {
       return requestPromise({
         method: 'get',
-        url: env.AUTH0_CALLBACK_URL+'/api/portfolio/' + id
+        url: 'http://localhost:3000/api/portfolio/' + id
       }).then(result => {
         return result
       }).catch(error => {
@@ -78,7 +78,7 @@ const checkLoggedIn = async (user) => {
     const ledger = async () => {
       return requestPromise({
         method: 'get',
-        url: env.AUTH0_CALLBACK_URL+'/api/ledger/' + id
+        url: 'http://localhost:3000/api/ledger/' + id
       }).then(result => {
         return result
       }).catch(error => {
