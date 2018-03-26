b10cf85d9b95ccb7e2187131966b6cab76f34446
Nema Darban <nema.darban@gmail.com>
2018-03-26 11:53:40 -0700

    local environment url

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ var lock = new Auth0Lock('aJPXuic5MPSYv35id6DoDxl7ZJJP2rtT', 'devious.auth0.com'
   auth: {
   	domain: 'devious.auth0.com',
   	audience: 'https://devious.auth0.com/userinfo',
-    redirectUrl: (process.env.AUTH0_CALLBACK_URL + '/callback' || 'http://localhost:5000/callback'),
+    redirectUrl: (process.env.AUTH0_CALLBACK_URL + '/callback' || 'http://localhost:3000/callback'),
     responseType: 'code',
     params: {
       scope: 'openid profile' // Learn about scopes: https://auth0.com/docs/scopes
